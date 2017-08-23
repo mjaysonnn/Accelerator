6cee6c748fcbbd077b8d102439aed849aa52aa35
Myungjun Son <mjs8612@gmail.com>
2017-08-23 19:39:17 +0900

    5번째 실습 이미지 회전

---
+++ image_rotation/image_rotation.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <CL/cl.h>
+#include "bmpfuncs.h"
+
+static float theta = 3.14159/6;
+
+void rotate(float *input_image, float *output_image, int image_width, int image_height,
+            float sin_theta, float cos_theta);
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("Usage: %s <src file> <dest file>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  float sin_theta = sinf(theta);
+  float cos_theta = cosf(theta);
+
+  int image_width, image_height;
+  float *input_image = readImage(argv[1], &image_width, &image_height);
+  float *output_image = (float*)malloc(sizeof(float) * image_width * image_height);
+  rotate(input_image, output_image, image_width, image_height, sin_theta, cos_theta);
+  storeImage(output_image, argv[2], image_height, image_width, argv[1]);
+  return 0;
+}
+
+#define CHECK_ERROR(err) \
+  if (err != CL_SUCCESS) { \
+    printf("[%s:%d] OpenCL error %d\n", __FILE__, __LINE__, err); \
+    exit(EXIT_FAILURE); \
+  }
+
+char *get_source_code(const char *file_name, size_t *len) {
+  char *source_code;
+  size_t length;
+  FILE *file = fopen(file_name, "r");
+  if (file == NULL) {
+    printf("[%s:%d] Failed to open %s\n", __FILE__, __LINE__, file_name);
+    exit(EXIT_FAILURE);
+  }
+
+  fseek(file, 0, SEEK_END);
+  length = (size_t)ftell(file);
+  rewind(file);
+
+  source_code = (char *)malloc(length + 1);
+  fread(source_code, length, 1, file);
+  source_code[length] = '\0';
+
+  fclose(file);
+
+  *len = length;
+  return source_code;
+}
+
+void rotate(float *input_image, float *output_image, int image_width, int image_height,
+            float sin_theta, float cos_theta) {
+  // TODO
+    cl_platform_id platform;
+
+    cl_device_id device;
+
+    cl_context context;
+
+    cl_command_queue queue;
+
+    cl_program program;
+
+    char *kernel_source;
+
+    size_t kernel_source_size;
+
+    cl_kernel kernel;
+
+    cl_int err;
+
+    err = clGetPlatformIDs(1, &platform, NULL);
+
+    CHECK_ERROR(err);
+
+    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    CHECK_ERROR(err);
+
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    CHECK_ERROR(err);
+
+    queue = clCreateCommandQueue(context, device, 0, &err);
+    CHECK_ERROR(err);
+
+    kernel_source = get_source_code("kernel.cl", &kernel_source_size);
+    program = clCreateProgramWithSource(context, 1, (const char **) &kernel_source, &kernel_source_size, &err);
+
+    CHECK_ERROR(err);
+
+    err = clBuildProgram(program, 1, &device, "", NULL, NULL);
+    if (err == CL_BUILD_PROGRAM_FAILURE) {
+        size_t log_size;
+        char *log;
+
+        err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+        CHECK_ERROR(err);
+
+        log = (char*)malloc(log_size+1);
+
+        err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+
+        CHECK_ERROR(err);
+
+        log[log_size] = '\0';
+        printf("Compiler error:\n%s\n", log);
+        free(log);
+        exit(0);
+    }
+    CHECK_ERROR(err);
+
+    kernel = clCreateKernel(program, "img_rotate", &err);
+    CHECK_ERROR(err);
+
+    size_t image_size = sizeof(float)*image_width*image_height;
+    cl_mem src, dest;
+    src= clCreateBuffer(context, CL_MEM_READ_ONLY, image_size, NULL, &err);
+    CHECK_ERROR(err);
+    dest = clCreateBuffer(context, CL_MEM_READ_WRITE, image_size, NULL, &err);
+    CHECK_ERROR(err);
+
+    err = clEnqueueWriteBuffer(queue, src, CL_FALSE, 0, image_size, input_image, 0, NULL, NULL);
+    CHECK_ERROR(err);
+
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &dest);
+    CHECK_ERROR(err);
+    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &src);
+    CHECK_ERROR(err);
+    err = clSetKernelArg(kernel, 2, sizeof(cl_int), &image_width);
+    CHECK_ERROR(err);
+    err = clSetKernelArg(kernel, 3, sizeof(cl_int), &image_height);
+    CHECK_ERROR(err);
+    err = clSetKernelArg(kernel, 4, sizeof(cl_float), &sin_theta);
+    CHECK_ERROR(err);
+    err = clSetKernelArg(kernel, 5, sizeof(cl_float), &cos_theta);
+    CHECK_ERROR(err);
+
+    size_t global_size[2] = {image_width, image_height};
+    size_t local_size[2] = {16, 16};
+    global_size[0] =(global_size[0]+local_size[0]-1)/local_size[0]*local_size[0];
+    global_size[1] =(global_size[1]+local_size[1]-1)/local_size[1]*local_size[1];
+
+    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_size, local_size, 0, NULL, NULL);
+    CHECK_ERROR(err);
+
+    err = clEnqueueReadBuffer(queue, dest, CL_TRUE, 0, image_size, output_image, 0, NULL, NULL);
+    CHECK_ERROR(err);
+
+    clReleaseMemObject(src);
+    clReleaseMemObject(dest);
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+}
---
+++ image_rotation/kernel.cl
@@ -0,0 +1,21 @@
+__kernel void img_rotate(__global float * dest_data,
+			                   __global float* src_data,
+			                   int W, int H,
+			                   float sinTheta, float cosTheta)
+{
+  int dest_x = get_global_id(0); // global size의 첫번째 값
+  int dest_y = get_global_id(1); // 출력 이미지는 데이터 디펜던스가 없음 
+  if (dest_x >= W || dest_y >= H) return;
+  float x0 = W / 2.0f;  좌표로 보면 점의 한 가운데 x축
+  float y0 = H / 2.0f;  좌표로 보면 점의 한 가운데 y축
+  float xOff = dest_x - x0; (x2,y2)는 (x0, y0)를 기준으로 (x1,y1)를 theta 만큼 회전함 점
+  float yOff = dest_y - y0;
+  int src_x = (int)(xOff*cosTheta + yOff*sinTheta + x0);  // (src_x,srx_y)는 (x0, y0)를 기준으로 (xOff,yOff)를 theta 만큼 움직인 점
+  int src_y = (int)(yOff*cosTheta + xOff*sinTheta + y0);
+  if((src_x >= 0) && (src_x < W ) && (src_y >= 0) && (src_y < H)) {
+  dest_data[dest_y*W+dest_x] = src_data[src_y*W + src_x]; //병렬처리를 하기 떄문에 W까지 한꺼번에 한다. x1과 y1은 제일 끝 부분으로 처리한다.
+ }
+ else {
+  dest_data[dest_y*W+dest_x] = 0.0f;
+ }
+}
