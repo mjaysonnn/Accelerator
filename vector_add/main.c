825d0772578217cadd75f89e3067867f499f03b0
Myungjun Son <mjs8612@gmail.com>
2017-08-29 00:46:30 +0900

    use Profiling to measure kernel execution time

--- vector_add/main.c
+++ vector_add/main.c
@@ -60,6 +60,12 @@ int main() {
 
     cl_int err;
 
+	int i;
+	cl_event kernel_event;
+	cl_ulong time_start, time_end;
+
+    printf("Program : C[163840000] = A[163840000] + B[163840000]\n\n");
+
     err = clGetPlatformIDs(1, &platform, NULL);
 
     CHECK_ERROR(err);
@@ -70,7 +76,7 @@ int main() {
     context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
     CHECK_ERROR(err);
 
-    queue = clCreateCommandQueue(context, device, 0, &err);
+    queue = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
     CHECK_ERROR(err);
 
     kernel_source = get_source_code("kernel.cl", &kernel_source_size);
@@ -106,11 +112,11 @@ int main() {
     //                const char *    /* kernel_name */,
     //                cl_int *        /* errcode_ret */)
 
-    int *A = (int*) malloc(sizeof(int) * 16384);
-    int *B = (int*) malloc(sizeof(int) * 16384);
-    int *C = (int*) malloc(sizeof(int) * 16384);
+    int *A = (int*) malloc(sizeof(int) * 163840000);
+    int *B = (int*) malloc(sizeof(int) * 163840000);
+    int *C = (int*) malloc(sizeof(int) * 163840000);
 
-    for (i = 0; i < 16384; i++) { // 초기화
+    for (i = 0; i < 163840000; i++) { // 초기화
         A[i] = rand() % 100;
         B[i] = rand() % 100;
     }
@@ -123,7 +129,7 @@ int main() {
     4. 커널 함수에서 그러면 일반적인 배열을 쓰듯이 값을 일고 쓴다.
     5. 마지막으로 버퍼 오브젝트는 메모리 전송 커맨드를 이용해 호스트 프로그램이 데이터를 읽게 한다.
     */
-    bufA = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * 16384, NULL, &err);  CHECK_ERROR(err);
+    bufA = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * 163840000, NULL, &err);  CHECK_ERROR(err);
 
     // clCreateBuffer(cl_context   /* context */,
     //                cl_mem_flags /* flags */,     // 여러가지 flag를 줄 수있다. CL_MEM_READ_WRITE, MEM_READ_ONLY
@@ -131,26 +137,26 @@ int main() {
     //                void *       /* host_ptr */,
     //                cl_int *     /* errcode_ret */)
 
-    bufB = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * 16384, NULL, &err); CHECK_ERROR(err);
+    bufB = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * 163840000, NULL, &err); CHECK_ERROR(err);
     
-    bufC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * 16384, NULL, &err); CHECK_ERROR(err);
-
-    
-
-    double start_time = get_time();
-
-    err = clEnqueueWriteBuffer(queue, bufA, CL_FALSE, 0, sizeof(int) * 16384, A, 0, NULL, NULL); CHECK_ERROR(err);
-
-    double end_time = get_time();
-    printf("buf A -> to Device: %f sec\n", end_time - start_time);
+    bufC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * 163840000, NULL, &err); CHECK_ERROR(err);
 
     
-    double start_time = get_time();
 
-    err = clEnqueueWriteBuffer(queue, bufB, CL_FALSE, 0, sizeof(int) * 16384, B, 0, NULL, NULL); CHECK_ERROR(err);
+    double start0 = get_time();
 
-    double end_time = get_time();
-    printf("bufB -> Device: %f sec\n", end_time - start_time);
+    err = clEnqueueWriteBuffer(queue, bufA, CL_TRUE, 0, sizeof(int) * 163840000, A, 0, NULL, NULL); CHECK_ERROR(err);
+
+    double end0 = get_time();
+    printf("bufA -> Device: %f sec\n\n", end0 - start0);
+
+    
+    double start1 = get_time();
+
+    err = clEnqueueWriteBuffer(queue, bufB, CL_TRUE, 0, sizeof(int) * 163840000, B, 0, NULL, NULL); CHECK_ERROR(err);
+
+    double end1 = get_time();
+    printf("bufB -> Device: %f sec\n\n", end1 - start1);
 
     err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufA); CHECK_ERROR(err);
 
@@ -158,35 +164,43 @@ int main() {
 
     err = clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufC); CHECK_ERROR(err);
 
-    size_t global_size = 16384;
-    size_t local_size = 256;
+    size_t global_size = 163840000;
+    size_t local_size = 32;
 
     
-    double start_time = get_time();
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL); CHECK_ERROR(err);
-    double end_time = get_time();
-    printf("Kernel Execution: %f sec\n", end_time - start_time);
+    double start2 = get_time();
+    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, &kernel_event); CHECK_ERROR(err);
+	err = clFinish(queue); CHECK_ERROR(err);
+	//err = clFlush(queue); CHECK_ERROR(err);
+	clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &time_start, NULL);
+	clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &time_end, NULL);
+
+	printf("Kernel Execution Time: %lu ns\n\n", time_end - time_start);
+   
+	//double end2 = get_time();
+    //printf("Kernel Execution: %f sec\n\n", end2 - start2);
     
 
-    double start_time = get_time();
-    err = clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0, sizeof(int) * 16384, C, 0, NULL, NULL); CHECK_ERROR(err);
-    double end_time = get_time();
-    printf("BUF C -> C: %f sec\n", end_time - start_time);
+    double start3 = get_time();
+    err = clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0, sizeof(int) * 163840000, C, 0, NULL, NULL); CHECK_ERROR(err);
+    //err = clFinish(queue);
+	double end3 = get_time();
+    printf("BUFC -> C: %f sec\n\n", end3 - start3);
 
-    double start_time = get_time();
-    clFinish(queue);
-    double end_time = get_time();
-    printf("CL_FINISH: %f sec\n", end_time - start_time);
+    double start4 = get_time();
+    err = clFinish(queue);
+    double end4 = get_time();
+    printf("CL_FINISH: %f sec\n\n", end4 - start4);
 
     
 
-    for (i = 0; i < 16384; i++) {
+    for (i = 0; i < 163840000; i++) {
         if (A[i] + B[i] != C[i]) {
             printf("Verification failed! A[%d] = %d, B[%d] = %d , C[%d] = %d", i, A[i], i, B[i], i, C[i]);
             break;
         }
     }
-    else (i == 16384)
+    if (i == 163840000)
     {
         printf("Verification success!\n");
     }
