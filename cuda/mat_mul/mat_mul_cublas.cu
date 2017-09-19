4ebf5ba25e9e9f14dce8a983457460d7defe956d
Myungjun Son <mjs8612@gmail.com>
2017-09-19 22:53:46 +0900

    change the code to let non-square matrix can be used

--- cuda/mat_mul/mat_mul_cublas.cu
+++ cuda/mat_mul/mat_mul_cublas.cu
@@ -7,51 +7,53 @@ if (err != CUBLAS_STATUS_SUCCESS) \
     {\
         printf("[%s:%d] CUBLAS error %d\n", __FILE__, __LINE__, err);\
         exit(EXIT_FAILURE);\
-    } 
+    }
 
 void mat_mul_cublas(float *A, float *B, float *C,
                     int ROW_A, int COL_A, int COL_B) {
-  
-  float *d_A, *d_B, *d_C, *d_trC;
+
+  float *d_A, *d_B, *d_C;
+  float *d_trC;
   float one = 1;
   float zero = 0;
-  
-  int N = ROW_A;
-  
-  
 
-  if(ROW_A != COL_A || ROW_A != COL_B)
-  {
-    printf("Support Square Matrix Only!\n");
-   exit(EXIT_FAILURE);
-  }
+  int M = ROW_A;
+  int K = COL_A;
+  int N = COL_B;
+
+
+  //if(ROW_A != COL_A || ROW_A != COL_B)
+  //{
+  //  printf("Support Square Matrix Only!\n");
+  // exit(EXIT_FAILURE);
+  //}
   /******************** TODO *********************/
-  
+
   cublasHandle_t handle;
   cublasStatus_t status;
 
   status = cublasCreate(&handle);
   CHECK_CUBLAS_ERROR(status);
 
-  cudaMalloc(&d_A, sizeof(float)*N*N);
-  cudaMalloc(&d_B, sizeof(float)*N*N);
-  cudaMalloc(&d_C, sizeof(float)*N*N);
-  cudaMalloc(&d_trC, sizeof(float)*N*N);
+  cudaMalloc(&d_A, sizeof(float)*M*K);
+  cudaMalloc(&d_B, sizeof(float)*K*N);
+  cudaMalloc(&d_C, sizeof(float)*M*N);
+  cudaMalloc(&d_trC, sizeof(float)*N*M);
 
 
-  status = cublasSetMatrix(N, N, sizeof(float), A, N, d_A, N);
+  status = cublasSetMatrix(M, K, sizeof(float), A, M, d_A, M);
   CHECK_CUBLAS_ERROR(status);
 
-  status = cublasSetMatrix(N, N, sizeof(float), B, N, d_B, N);
+  status = cublasSetMatrix(K, N, sizeof(float), B, K, d_B, K);
   CHECK_CUBLAS_ERROR(status);
 
-  status = cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, N, N, N, &one, d_A, N, d_B, N, &zero, d_trC, N);  // Non_Blocking 이다.
+  status = cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, M, N, K, &one, d_A, K, d_B, N, &zero, d_trC, M);  // Non_Blocking 이다.
   CHECK_CUBLAS_ERROR(status);
 
-  status = cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_T, N, N, &one, d_trC, N, &zero, d_trC, N, d_C, N);
+  status = cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_T, M, N, &one, d_trC, N, &zero, d_trC, M, d_C, M);
   CHECK_CUBLAS_ERROR(status);
 
-  status = cublasGetMatrix(N, N, sizeof(float), d_C, N, C, N);
+  status = cublasGetMatrix(M, N, sizeof(float), d_C, M, C, M);
   CHECK_CUBLAS_ERROR(status);
 
   status = cublasDestroy(handle);
@@ -60,4 +62,3 @@ void mat_mul_cublas(float *A, float *B, float *C,
   cudaFree(d_B);
   cudaFree(d_C);
 }
-
